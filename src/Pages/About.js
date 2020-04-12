97f2e3aecb8fbbd3ab0f1c11b530783db99bd67e
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-12 13:38:15 -0400

    setup about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,20 @@ import React from 'react';
 import './About.css';
 
 const About = () => {
-    return( <h1 >About </h1>
+    return( <section class="section-about" id = "about">
+    <div class="u-center-text u-margin-bottom-8">
+        <h2 class="heading-secondary">
+            Exciting Facts About Me
+        </h2>
+        <p class ="paragraph">Hi,</p>
+        <p class ="paragraph">I am Bipanjeet Kaur Gill.</p><br/>
+        <p class ="paragraph">
+            I am passionate to play with objects.I am working as Front End Web Developer in Decto Canada Ltd and  ready to learn new things.I love to craft interactive UI and ability to architect the models for database, maintain and retrieving data by ORM, structuring webpages through MVC and make them responsive.Desire to stay up-to-date on design and development trends, Flexible for working hours. I am dedicated to solving complex business problems and forward-thinking solutions.</p>
+        <p class ="paragraph">
+            I have an experience as software programmer for ten years in G-Tech Automation Pvt.Ltd.India and Certified by <b>University of Toronto</b> as Full Stack Web Developer.Knowledge of Agile / SCRUM & SDLC, and have interest to explore more about AWS, AZURE and GCP.
+        </p>
+        </div>
+</section>
     )
 }
-export default About;
+export default About 
